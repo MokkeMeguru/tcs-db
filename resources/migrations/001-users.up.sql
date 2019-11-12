18314065cb4d0b6081962ecdbd2e5b75b805652f
Mokke Meguru <meguru.mokke@gmail.com>
2019-11-12 14:16:37 +0900

    Update 001-users.up.sql

--- resources/migrations/001-users.up.sql
+++ resources/migrations/001-users.up.sql
@@ -1,10 +1,9 @@
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
-  email VARCHAR(255),
+  email VARCHAR(255) UNIQUE,
   password VARCHAR(255) NOT NULL,
   created_at TIMESTAMP default CURRENT_TIMESTAMP,
   updated_at TIMESTAMP,
-  is_deleted BOOLEAN NOT NULL default FALSE,
-  UNIQUE(name, password)
+  is_deleted BOOLEAN NOT NULL default FALSE
 );
